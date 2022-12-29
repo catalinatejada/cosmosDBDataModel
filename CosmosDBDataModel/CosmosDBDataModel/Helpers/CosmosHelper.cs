69912b5991efe28a7c91f4112668700def278748
Catalina Tejada <ctejada@microsoft.com>
2022-12-29 12:59:10 +0100

    add models and cosmosHelper

---
+++ CosmosDBDataModel/CosmosDBDataModel/Helpers/CosmosHelper.cs
@@ -0,0 +1,122 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using CosmosDBDataModel.Config;
+using IDataObject = CosmosDBDataModel.Model.IDataObject;
+
+
+
+namespace CosmosDBDataModel.Helpers
+{
+    public class CosmosHelpers
+    {
+
+        public static async Task<ItemResponse<T>> InsertItem<T>(string databaseName, string containerName, T Element)
+        {
+            using (CosmosClient client = new(
+                    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+                    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+            ))
+            {
+                var db = client.GetDatabase(databaseName);
+                var container = db.GetContainer(containerName);
+                try
+                {
+                    var response = await container.CreateItemAsync<T>(Element);
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        public static async Task<TransactionalBatchOperationResult<T>> InsertTransacctionalBatch<T>(string databaseName, string containerName, List<T> Elements) where T : IDataObject
+        {
+            using CosmosClient client = new(
+                    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+                    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+            );
+
+            var db = client.GetDatabase(databaseName);
+            var container = db.GetContainer(containerName);
+            PartitionKey partitionKey = new PartitionKey(Elements[0].partitionKey);
+            TransactionalBatch batch = container.CreateTransactionalBatch(partitionKey);
+            foreach (var item in Elements)
+                batch.CreateItem<T>(item);
+            using TransactionalBatchResponse response = await batch.ExecuteAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Inserted");
+                TransactionalBatchOperationResult<T> productResponse;
+                return response.GetOperationResultAtIndex<T>(0);
+            }
+            else
+            {
+                Console.WriteLine($"ERROR: {response.StatusCode} {response.ErrorMessage}");
+            }
+
+            return null;
+        }
+
+        public static async Task CreateContainer(string databaseName, string containerName, string partitionKeyPath)
+        {
+            using CosmosClient client = new(
+                    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+                    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+            );
+
+            var db = client.GetDatabase(databaseName);
+            var containercreated = await db.CreateContainerAsync(
+                                            id: containerName,
+                                            partitionKeyPath: partitionKeyPath);
+        }
+
+        public static async Task DeleteContainer(string databaseName, string containerName)
+        {
+            using CosmosClient client = new(
+                     accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+                     authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+             );
+            var db = client.GetDatabase(databaseName);
+            var container = db.GetContainer(containerName);
+
+            await container.DeleteContainerAsync();
+
+        }
+        public static async Task<CosmosTestRun<T>> QueryItems<T>(string databaseName, string containerName, QueryDefinition query)
+        {
+            List<T> collection = new List<T>();
+            double consumedRUs = 0;
+            using CosmosClient client = new(
+                    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+                    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+            );
+
+            var db = client.GetDatabase(databaseName);
+            var container = db.GetContainer(containerName);
+            using FeedIterator<T> feed = container.GetItemQueryIterator<T>(query);
+
+            while (feed.HasMoreResults)
+            {
+                FeedResponse<T> response = await feed.ReadNextAsync();
+                consumedRUs += response.RequestCharge;
+                foreach (T item in response)
+                {
+                    collection.Add(item);
+                }
+            }
+            return CosmosTestRun<T>.SaveInTestRun(collection, consumedRUs);
+        }
+
+    }
+}
--- CosmosDBDataModel/CosmosDBDataModel/Program.cs
+++ CosmosDBDataModel/CosmosDBDataModel/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CosmosDBDataModel.Config;
+using CosmosDBDataModel.Helpers;
+using CosmosDBDataModel.Model;
+using CosmosDBDataModel.Seeder;
+using Microsoft.Azure.Cosmos;
+
 Console.WriteLine("Hello, World!");
+
+//await OrdersSeeder.SeedData();
+
+
+CosmosTestRun <LineaPedido> lineapedido = await CosmosHelpers.QueryItems<LineaPedido>(databaseName: "cosmospocindi", containerName: "Pedidos_C", query: new QueryDefinition("SELECT * from c where c.type = \"orderline\""));
+CosmosTestRun<Pedido> pedido = await CosmosHelpers.QueryItems<Pedido>(databaseName: "cosmospocindi", containerName: "Pedidos_C", query: new QueryDefinition("SELECT * from c where c.type = \"order\""));
+
+Console.WriteLine("Pedido:");
+
+Console.WriteLine("RU:" + pedido._requestsConsumed);
+Console.WriteLine("total items: " + pedido._items.Count());
+
+
+Console.WriteLine("Linea Pedido:");
+
+Console.WriteLine("RU:" + lineapedido._requestsConsumed);
+Console.WriteLine("total items: " + lineapedido._items.Count());
+
